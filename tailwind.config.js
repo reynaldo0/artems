9b3cf57f49b1280ea57a5974395a49bfccb2da39
reynaldo0 <reynaldooyusellino@gmail.com>
2024-08-25 11:42:32 +0700

    add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary : "#7B7643"
+      }
+    },
   },
   plugins: [],
 }
