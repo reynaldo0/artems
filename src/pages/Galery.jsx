176769b72e2be7dc236340abd012ae92042e4c8b
--global <--global>
2024-08-29 08:40:34 +0700

    adding button

--- src/pages/Galery.jsx
+++ src/pages/Galery.jsx
@@ -44,11 +44,25 @@ const Gallery = () => {
           {/* Text beside portrait image */}
           <div className="flex flex-col pt-5 md:pt-20 md:pl-16 w-full md:w-3/5">
             <h2 className="text-3xl font-bold mb-4 md:text-7xl">Leonardo
-            Da Vinci</h2>
+              Da Vinci</h2>
             <p className="text-lg md:text-xl text-gray-600">
               This gallery features some of the most renowned artworks including the Mona Lisa, The Last Supper, and works by Leonardo Da Vinci. Each piece is a masterpiece with its unique history and significance in the art world.
             </p>
+            <div className="flex flex-row pt-10 md:pt-20 gap-12 md:gap-40">
+              <div className="flex flex-col">
+                <p>Bom</p>
+                <p className="font-bold text-lg">April 15, 1452</p>
+              </div>
+              <div className="flex flex-col">
+                <p>Died</p>
+                <p className="font-bold text-lg">May 2, 1519</p>
+              </div>
+            </div>
+            <div className="flex pt-10">
+              <button className="bg-transparent border-2 border-primary hover:bg-primary hover:text-white text-primary rounded-full px-5 py-2">Read More</button>
+            </div>
           </div>
+
         </div>
       </div>
     </div>
