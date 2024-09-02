b11358246c812b555e024cbf02875945ac8e2cea
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-02 14:13:01 +0700

    fix card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,10 +8,10 @@ const Card = ({ title, description, image }) => {
   };
 
   return (
-    <div className="relative max-w-lg mx-auto bg-white rounded-t-3xl shadow-lg overflow-hidden w-[400px]">
+    <div className="relative max-w-lg mx-auto bg-white rounded-3xl shadow-lg overflow-hidden md:w-[400px]">
       <img
         src={image}
-        className="w-full h-64 object-cover" // Apply rounded corners only to the top
+        className="w-full h-52 md:h-60 object-cover" // Apply rounded corners only to the top
         alt={title}
       />
       <div className="p-6"> {/* Increased padding for larger content area */}
