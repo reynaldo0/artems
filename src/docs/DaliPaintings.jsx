9c03d362443803b473ad6f0c5562c588039a36d2
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-04 20:44:03 +0700

    upd konten

--- src/docs/DaliPaintings.jsx
+++ src/docs/DaliPaintings.jsx
@@ -1,99 +1,74 @@
 const DaliPaintings = [
     {
-      year: "1470s",
+      year: "1930s",
       title: "INI LEONARDO ANJAY MABAR",
       description: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1470s",
+      year: "1930s",
       title: "Adoration of The Magi (1471)",
       description: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1470s",
+      year: "1930s",
       title: "Virgins of The Rocks (1473)",
       description: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1470s",
+      year: "1930s",
       title: "Virgins of The Rocks (1473)",
       description: "Description here...",
       image: "/gambar1.png",
     },
-    //1480s
+    //1940s
     {
-      year: "1480s",
+      year: "1940s",
       title: "St. Jerome In The Wilderness (1480)",
       description: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1480s",
+      year: "1940s",
       title: "Adoration of The Magi (1481)",
       description: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1480s",
+      year: "1940s",
       title: "Virgins of The Rocks (1483)",
       description: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1480s",
+      year: "1940s",
       title: "Virgins of The Rocks (1483)",
       description: "Description here...",
       image: "/gambar1.png",
     },
-    //1490s
+    //1950s
     {
-      year: "1490s",
+      year: "1950s",
       title: "St. Jerome In The Wilderness (1490)",
       description: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1490s",
+      year: "1950s",
       title: "Adoration of The Magi (1481)",
       d9scription: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1490s",
+      year: "1950s",
       title: "Virgins of The Rocks (1483)",
       description: "Description here...",
       image: "/gambar1.png",
     },
     {
-      year: "1490s",
-      title: "Virgins of The Rocks (1483)",
-      description: "Description here...",
-      image: "/gambar1.png",
-    },
-    //1500s
-    {
-      year: "1500s",
-      title: "St.  Jerome In The Wilderness (1500)",
-      description: "Description here...",
-      image: "/gambar1.png",
-    },
-    {
-      year: "1500s",
-      title: "Adoration of The Magi (1481)",
-      d9scription: "Description here...",
-      image: "/gambar1.png",
-    },
-    {
-      year: "1500s",
-      title: "Virgins of The Rocks (1483)",
-      description: "Description here...",
-      image: "/gambar1.png",
-    },
-    {
-      year: "1500s",
+      year: "1950s",
       title: "Virgins of The Rocks (1483)",
       description: "Description here...",
       image: "/gambar1.png",
