6342d2e6462ea9b622a3f0c5c244df525b848e9b
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-11 19:22:27 +0700

    up

--- src/docs/GalleryAbout.jsx
+++ src/docs/GalleryAbout.jsx
@@ -1,49 +1,55 @@
 const galleryAboutData = [
-    {
-      path: "/alifian",
-      images: [
-        { src: "/46.jpg", title: "SMKN 46 JAKARTA" },
-        { src: "/painters/leonardo/supper.png", title: "The Last Supper" }
-      ],
-      mainImage: { src: "/about/2.jpg", title: "Alifian Abdiel Rafif" },
-      description: "a passionate programmer from SMKN 46 who thrives on exploring the dynamic world of JavaScript and cutting-edge technology. With a deep curiosity and a relentless drive for innovation,  efficient web applications and diving into the latest tech trends like AI, IoT etc. Combines technical expertise with a collaborative mindset, making a significant impact in every project. Driven by a commitment to continuous learning and a love for tech, my self is on a mission to shape the future of digital experiences.",
-      born: "February 27, 2007",
-      socialLinks: [
-        { icon: "Instagram", url: "https://instagram.com/vincent" },
-        { icon: "Linkedin", url: "https://linkedin.com/vincent" },
-        { icon: "Github", url: "https://vincentvangogh.com" }
-      ]
+  {
+    path: "/alifian",
+    images: [
+      { src: "/46.jpg", title: "SMKN 46 JAKARTA" },
+      { src: "/painters/leonardo/supper.png", title: "The Last Supper" },
+    ],
+    mainImage: { src: "/about/2.jpg", title: "Alifian Abdiel Rafif" },
+    description:
+      "a passionate programmer from SMKN 46 who thrives on exploring the dynamic world of JavaScript and cutting-edge technology. With a deep curiosity and a relentless drive for innovation,  efficient web applications and diving into the latest tech trends like AI, IoT etc. Combines technical expertise with a collaborative mindset, making a significant impact in every project. Driven by a commitment to continuous learning and a love for tech, my self is on a mission to shape the future of digital experiences.",
+    born: "February 27, 2007",
+    socialLinks: [
+      { icon: "Instagram", url: "https://instagram.com/vincent" },
+      { icon: "Linkedin", url: "https://linkedin.com/vincent" },
+      { icon: "Github", url: "https://vincentvangogh.com" },
+    ],
+  },
+  {
+    path: "/salman",
+    images: [
+      { src: "/46.jpg", title: "SMKN 46 JAKARTA" },
+      { src: "/painters/vincent/gal2.png", title: "The Starry Night" },
+    ],
+    mainImage: { src: "/about/1.jpg", title: "Muhammad Salman Alfarizi" },
+    description:
+      "Dutch painter, generally considered the greatest after Rembrandt van Rijn, and one of the greatest of the Post-Impressionists.",
+    born: "March 30, 1853",
+    socialLinks: [
+      { icon: "Instagram", url: "https://instagram.com/vincent" },
+      { icon: "Linkedin", url: "https://linkedin.com/vincent" },
+      { icon: "Github", url: "https://vincentvangogh.com" },
+    ],
+  },
+  {
+    path: "/aldo",
+    images: [
+      { src: "/46.jpg", title: "SMKN 46 JAKARTA" },
+      { src: "/painters/pablo/gal2.png", title: "The Weeping Woman" },
+    ],
+    mainImage: {
+      src: "/painters/pablo/pablo.png",
+      title: "Reynaldo Yusellino",
     },
-    {
-      path: "/salman",
-      images: [
-        { src: "/46.jpg", title: "SMKN 46 JAKARTA" },
-        { src: "/painters/vincent/gal2.png", title: "The Starry Night" }
-      ],
-      mainImage: { src: "/about/1.jpg", title: "Muhammad Salman Alfarizi" },
-      description: "Dutch painter, generally considered the greatest after Rembrandt van Rijn, and one of the greatest of the Post-Impressionists.",
-      born: "March 30, 1853",
-      socialLinks: [
-        { icon: "Instagram", url: "https://instagram.com/vincent" },
-        { icon: "Linkedin", url: "https://linkedin.com/vincent" },
-        { icon: "Github", url: "https://vincentvangogh.com" }
-      ]
-    },
-    {
-      path: "/aldo",
-      images: [
-        { src: "/46.jpg", title: "SMKN 46 JAKARTA" },
-        { src: "/painters/pablo/gal2.png", title: "The Weeping Woman" }
-      ],
-      mainImage: { src: "/painters/pablo/pablo.png", title: "Reynaldo Yusellino" },
-      description: "A Spanish painter, sculptor, and one of the most influential artists of the 20th century, known for co-founding the Cubist movement.",
-      born: "October 25, 1881",
-      socialLinks: [
-        { icon: "Instagram", url: "https://instagram.com/vincent" },
-        { icon: "Linkedin", url: "https://linkedin.com/vincent" },
-        { icon: "Github", url: "https://vincentvangogh.com" }
-      ]
-    }
-  ];
+    description:
+      "A passionate and driven programmer from SMKN 46 Jakarta, with solid experience in web development and cybersecurity. Always eager to learn new things and never backs down from a challenge. Skilled in collaborating with teams to create highly interactive, dynamic, and fully responsive websites across all devices.",
+    born: "June 07, 2007",
+    socialLinks: [
+      { icon: "Instagram", url: "https://instagram.com/vincent" },
+      { icon: "Linkedin", url: "https://linkedin.com/vincent" },
+      { icon: "Github", url: "https://vincentvangogh.com" },
+    ],
+  },
+];
 
-export default galleryAboutData;     
+export default galleryAboutData;
